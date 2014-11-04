c2ba69721b6f73a7e13516e1d687b332eee0343b
Nathan Levesque <rhysyngsun@gmail.com>
2014-11-04 15:00:19 -0500

    Latest

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Nathan Levesque'
 SITENAME = u"Rhysyngsun's Blog"
-SITEURL = 'http://rhysyngsun.com'
+SITEURL = ''
 
 PATH = 'content'
 
@@ -12,7 +12,7 @@ TIMEZONE = 'US/Eastern'
 
 DEFAULT_LANG = u'en'
 
-THEME = "/Users/nathan/pelican-themes/svbtle"
+THEME = "/Users/nathan/dev/oss/dev-random3"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -23,13 +23,25 @@ FILENAME_METADATA='(?P<date>\d{4}-\d{2}-\d{2})_(?P<slug>.*)'
 STATIC_PATHS = ['images', 'CNAME']
 
 # Blogroll
-LINKS = (('Twitter', 'https://twitter.com/rhysyngsun'),)
+LINKS = (('https://github.com/rhysyngsun', 'Github'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('https://twitter.com/rhysyngsun', 'Twitter'),)
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+PLUGIN_PATHS = ['/Users/nathan/dev/oss/pelican-plugins']
+PLUGINS = ["pelican-langcategory"]
+
+LANGUAGES= (('en','english','en'),)
+
+LANGUAGE_URL = '{lang}/'
+LANGUAGE_SAVE_AS = '{lang}/index.html'
+
+PAGE_URL = "{lang}/{slug}.html"
+PAGE_SAVE_AS = "{lang}/{slug}.html"
+PAGE_LANG_URL = "{lang}/{slug}.html"
+PAGE_LANG_SAVE_AS = "{lang}/{slug}.html"
